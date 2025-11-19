9729c0768e722e44b940f3e0c685cafe5498ee16
pavithraLakshmi-0312 <mgpmba@gmail.com>
2025-11-19 12:05:52 +0530

    Add initial book collection in Books.js

---
+++ Books.js
@@ -0,0 +1,34 @@
+let books = {
+  1: {
+    title: "Things Fall Apart",
+    author: "Achebe",
+    reviews: {}
+  },
+  2: {
+    title: "Fairy tales",
+    author: "Hans Christian Andersen",
+    reviews: {}
+  },
+  3: {
+    title: "The Divine Comedy",
+    author: "Dante Alighieri",
+    reviews: {}
+  },
+  4: {
+    title: "The Epic Of Gilgamesh",
+    author: "Unknown",
+    reviews: {}
+  },
+  5: {
+    title: "The Book Of Job",
+    author: "Unknown",
+    reviews: {}
+  },
+  6: {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    reviews: {}
+  }
+};
+
+module.exports = books;
